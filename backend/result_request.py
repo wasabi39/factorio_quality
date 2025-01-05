4982d7f146963a920c697c3521b9a94bd8068a76
wasabi39 <benjaminwaziridk@gmail.com>
2025-01-05 22:25:42 +0100

    adjusted pylint workflow slightly

--- backend/result_request.py
+++ backend/result_request.py
@@ -1,16 +1,19 @@
+"""
+Data model for returning the result from a simulation. 
+Saved as a Pydantic class in a separate file for two reasons:
+1) Makes it easier to create an API, if I ever decide to make one.
+2) Even if I never create an API, it makes the code cleaner, 
+more readable and helps us somewhat keep the code separated 
+into a frontend and a backend.
+Note: The counts are floats, because they're expected values,
+which aren't necessarily integers.
+"""
+
 from pydantic import BaseModel
-#Data model for returning the result from a simulation. 
-#Saved as a Pydantic class in a separate file for two reasons:
-#1) Makes it easier to create an API, if I ever decide to make one.
-#2) Even if I never create an API, it makes the code cleaner, 
-#more readable and helps us somewhat keep the code separated 
-#into a frontend and a backend.
-#Note: The counts are floats, because they're expected values,
-#which aren't necessarily integers.
+
 class ResultRequest(BaseModel):
     quality_1_count: float
     quality_2_count: float
     quality_3_count: float
     quality_4_count: float
     quality_5_count: float
-    
